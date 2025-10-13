e490c12aa3eb1c044a0846076b6747ca625bd40d
skywalkerfsx <skywalkerfsx@gmail.com>
2025-10-13 21:43:08 +0000

    v33

--- lab5/scripts/main.js
+++ lab5/scripts/main.js
@@ -4,9 +4,11 @@ const id2 = document.querySelector('#id2');
 const red = document.querySelector('#red');
 const green = document.querySelector('#green');
 const blue = document.querySelector('#blue');
+const inbox1 = document.querySelector('#inbox1');
 
 textchange.addEventListener("mouseover", passaby);
 textchange.addEventListener("mouseout", passaoff);
+textContent.addEventListener("keydown", kdown);
 
 document.querySelectorAll("button.color").forEach((b) => {
     b.onclick=function(){
@@ -17,10 +19,16 @@ document.querySelectorAll("button.color").forEach((b) => {
 
 
 
+
+
 function passaby() {
     textchange.textContent = "Obrigado por passares!";
 }
 
 function passaoff() {
     textchange.textContent = "Passa por aqui!";
+}
+
+function kdown(){
+    inbox1.style.color = "green";
 }
