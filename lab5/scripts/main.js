680b552db6c781782b87cbc6de04df7a59c03509
skywalkerfsx <skywalkerfsx@gmail.com>
2025-10-13 23:07:17 +0000

    V35

--- lab5/scripts/main.js
+++ lab5/scripts/main.js
@@ -5,13 +5,20 @@ const red = document.querySelector('#red');
 const green = document.querySelector('#green');
 const blue = document.querySelector('#blue');
 const inbox1 = document.querySelector('#inbox1');
-//const botaocor = document.querySelector('button.color');
+const colorir = document.querySelector('#cor');
+const submeter = document.querySelector("#submeter");
+const conta = document.querySelector("#conta");
+const valor = document.querySelector("#valor");
+
+let count = 0;
 
 textchange.addEventListener("mouseover", passaby);
 textchange.addEventListener("mouseout", passaoff);
 inbox1.addEventListener("keydown", kdown);
-//botaocor.addEventListener("click",botaored);
-
+inbox1.addEventListener("keyup", kup);
+inbox1.addEventListener("change", inputChange);
+submeter.addEventListener("click", colorirCaixa);
+conta.addEventListener("click", contagem);
 
 
 document.querySelectorAll("button.color").forEach((b) => {
@@ -21,9 +28,7 @@ document.querySelectorAll("button.color").forEach((b) => {
 
 })
 
-//function botaored(){
-//    id2.style.color = "red";
-//}
+
 
 
 
@@ -37,4 +42,21 @@ function passaoff() {
 
 function kdown(){
     inbox1.style.backgroundColor = "green";
+}
+
+function kup(){
+    inbox1.style.backgroundColor = "yellow";
+}
+
+function inputChange(){
+    inbox1.style.backgroundColor = "blue";
+}
+
+function colorirCaixa(){
+    colorir.style.backgroundColor = colorir.value;
+}
+
+function contagem(){
+    count++;
+    valor.textContent = count;
 }
