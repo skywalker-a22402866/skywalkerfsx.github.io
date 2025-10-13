ef004cd2d60267fe7df57ccd982f533b18acca86
skywalkerfsx <skywalkerfsx@gmail.com>
2025-10-13 22:06:50 +0000

    v34

--- lab5/scripts/main.js
+++ lab5/scripts/main.js
@@ -5,10 +5,14 @@ const red = document.querySelector('#red');
 const green = document.querySelector('#green');
 const blue = document.querySelector('#blue');
 const inbox1 = document.querySelector('#inbox1');
+//const botaocor = document.querySelector('button.color');
 
 textchange.addEventListener("mouseover", passaby);
 textchange.addEventListener("mouseout", passaoff);
-textContent.addEventListener("keydown", kdown);
+inbox1.addEventListener("keydown", kdown);
+//botaocor.addEventListener("click",botaored);
+
+
 
 document.querySelectorAll("button.color").forEach((b) => {
     b.onclick=function(){
@@ -17,7 +21,9 @@ document.querySelectorAll("button.color").forEach((b) => {
 
 })
 
-
+//function botaored(){
+//    id2.style.color = "red";
+//}
 
 
 
@@ -30,5 +36,5 @@ function passaoff() {
 }
 
 function kdown(){
-    inbox1.style.color = "green";
+    inbox1.style.backgroundColor = "green";
 }
