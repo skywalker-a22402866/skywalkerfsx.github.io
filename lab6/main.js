7632eff719ef9aa1cd9f41ab60fd6e9f9e93739a
skywalkerfsx <skywalkerfsx@gmail.com>
2025-10-27 20:14:22 +0000

    v37

--- lab6/main.js
+++ lab6/main.js
@@ -1,5 +1,5 @@
 //console.log(produtos)
-const section = document.querySelector('#products');
+const section = document.querySelector('#productos');
 
 addEventListener("DOMContentLoaded", (event) => { 
     carregarProdutos(produtos);
@@ -16,7 +16,18 @@ function carregarProdutos(produtos){
 function criarProduto(produto){
 
     const artigo = document.createElement('article')
-    artigo.textContent = produto.title;
+    //artigo.textContent = produto.title;
+    const imagem = document.createElement('img');
+    imagem.src = produto.image;     // busca a imagem do produto
+    imagem.alt = produto.title;     // texto alternativo
+    imagem.style.width = "150px";   // opcional: define tamanho
+
+    const titulo = document.createElement('h3');
+    titulo.textContent = produto.title;
+
+    const preco = document.createElement('p');
+    preco.textContent = `Preço: €${produto.price.toFixed(2)}`;
+    artigo.append(imagem, titulo, preco);
     section.append(artigo); 
 
 }
